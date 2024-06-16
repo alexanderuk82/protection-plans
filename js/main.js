9dd6d3dcb13ed6402d581114d1d138c291c7cef6
alexDev <info@alexandersstudio.uk>
2024-06-16 16:54:47 +0100

    Implement hero form functionality and styles, add coupleHero image

--- js/main.js
+++ js/main.js
@@ -5,4 +5,30 @@ document.addEventListener("DOMContentLoaded", function () {
 	closeBannerButton.addEventListener("click", function () {
 		promoTop.style.display = "none";
 	});
+
+	// Hero section boxes options
+
+	const labels = document.querySelectorAll(".custom-checkbox");
+
+	labels.forEach((label) => {
+		label.addEventListener("click", function () {
+			// Deselect all other checkboxes
+			labels.forEach((lbl) => {
+				lbl.querySelector("input").checked = false;
+				lbl.querySelector(".checkBoxIcon img").classList.add("hidden");
+			});
+
+			// Select the current checkbox
+			this.querySelector("input").checked = true;
+			this.querySelector(".checkBoxIcon img").classList.remove("hidden");
+
+			// Store the selected value in localStorage
+			const selectedValue = this.dataset.value;
+			localStorage.setItem("selectedInsuranceType", selectedValue);
+
+			// Redirect to another dynamic page
+			const nextPageUrl = `${selectedValue}.html`;
+			window.location.href = nextPageUrl;
+		});
+	});
 });
